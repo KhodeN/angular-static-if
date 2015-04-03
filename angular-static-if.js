3333407f37555771ad5e35d6c7efd4b37f2f133a
Artem Berezin <zkhoden@gmail.com>
2015-04-03 15:12:06 +0900

    Update angular-static-if.js

--- angular-static-if.js
+++ angular-static-if.js
@@ -1,27 +1,33 @@
-angular.module('angular-static-if', [])
-    .directive('ngStaticIf', ['$compile', function ($compile) {
-        return {
-            restrict: 'A',
-            priority: 1000,
-            terminal: true,
-            compile: function () {
-                return function ($scope, element, attrs) {
-                    var content = element.contents();
-                    var cachedValue = false;
+define(['angular'], function (angular) {
+    'use strict';
 
-                    $scope.$watch(attrs.ngStaticIf, function (value) {
-                        value = Boolean(value);
-                        if (value === cachedValue) {
-                            return;
-                        }
+    angular.module('ngStaticIf', [])
+        .directive('ngStaticIf', ['$compile', function ($compile) {
+            return {
+                restrict: 'A',
+                priority: 1000,
+                terminal: true,
+                compile: function () {
+                    return function ($scope, element, attrs) {
+                        var content = element.contents();
+                        var cachedValue = false;
 
-                        cachedValue = value;
-                        element.empty();
-                        if (value) {
-                            element.append($compile(content)($scope));
-                        }
-                    });
-                };
-            }
-        };
-    }]);
+                        $scope.$watch(attrs.ngStaticIf, function (value) {
+                            value = Boolean(value);
+                            if ( value === cachedValue ) {
+                                return;
+                            }
+
+                            cachedValue = value;
+                            element.empty();
+                            if ( value ) {
+                                element.append($compile(content)($scope));
+                            }
+                        });
+                    };
+                }
+            };
+        }]);
+
+    return 'ngStaticIf';
+});
